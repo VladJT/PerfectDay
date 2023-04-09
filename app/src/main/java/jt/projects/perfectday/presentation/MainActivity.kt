b3a1423e0aecf93dece913bb634dd86ac4e17580
EugeneZaukin <ezapach@yandex.ru>
2023-04-09 21:07:08 +0300

    Redraw main activity layout and add click listener on settings

--- app/src/main/java/jt/projects/perfectday/presentation/MainActivity.kt
+++ app/src/main/java/jt/projects/perfectday/presentation/MainActivity.kt
@@ -1,24 +1,19 @@
 package jt.projects.perfectday.presentation
 
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import jt.projects.model.DataModel
 import jt.projects.perfectday.R
 import jt.projects.perfectday.databinding.ActivityMainBinding
 import jt.projects.perfectday.presentation.calendar.CalendarFragment
-import jt.projects.perfectday.presentation.dialogs.ScheduleEventDialogFragment
 import jt.projects.perfectday.presentation.reminder.ReminderFragment
 import jt.projects.perfectday.presentation.settings.SettingsFragment
 import jt.projects.perfectday.presentation.today.TodayFragment
 import jt.projects.utils.network.OnlineStatusLiveData
 import jt.projects.utils.showSnackbar
 import org.koin.android.ext.android.getKoin
-import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -27,19 +22,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        if (savedInstanceState == null) {
-            replaceFragment(TodayFragment.newInstance())
-        }
-        initToolBar()
         initBottomNavView()
-        initFab()
+        setOnClickSettings()
         //    subscribeToNetworkStatusChange()
-    }
-
-    private fun initToolBar() {
-        binding.toolbar.title = getString(R.string.app_name)
-        setSupportActionBar(binding.toolbar)
     }
 
     private fun initBottomNavView() {
@@ -49,11 +34,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun initFab() {
-        binding.fabTop.setOnClickListener {
-            ScheduleEventDialogFragment
-                .newInstance(data = null)
-                .show(supportFragmentManager, ScheduleEventDialogFragment.TAG)
+    private fun setOnClickSettings() {
+        binding.layoutToolbar.btnSettings.setOnClickListener {
+            navigateToFragment(SettingsFragment(), isAddToBackStack = true)
         }
     }
 
@@ -65,40 +48,33 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_toolbar, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-
             R.id.menu_action_today -> {
-                replaceFragment(TodayFragment.newInstance())
+                navigateToFragment(TodayFragment.newInstance())
             }
 
             R.id.menu_action_reminder -> {
-                replaceFragment(ReminderFragment.newInstance())
+                navigateToFragment(ReminderFragment.newInstance())
             }
 
             R.id.menu_action_calendar -> {
-                replaceFragment(CalendarFragment.newInstance())
+                navigateToFragment(CalendarFragment.newInstance())
             }
-
-            R.id.menu_action_settings -> {
-                replaceFragment(SettingsFragment())
-            }
-
         }
         return super.onOptionsItemSelected(item)
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
+    private fun navigateToFragment(fragment: Fragment, isAddToBackStack: Boolean = false) {
+        val beginTransaction = supportFragmentManager.beginTransaction()
+        beginTransaction.replace(binding.fragmentContainer.id, fragment)
+
+        if (isAddToBackStack) {
+            beginTransaction.addToBackStack(null)
+        }
+        beginTransaction
             //.setCustomAnimations(R.animator.std_left, R.animator.std_right)
-            .replace(binding.fragmentContainer.id, fragment)
-            .commitNow()
+            .commit()
     }
 
     fun showProgress(progress: Int) {
