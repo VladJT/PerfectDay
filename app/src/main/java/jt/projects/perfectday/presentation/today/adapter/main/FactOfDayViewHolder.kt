ce3280759f198668e239548575281d47b0eeefe5
EugeneZaukin <ezapach@yandex.ru>
2023-04-13 20:03:29 +0300

    Add hide recycler view and fix layout  margins

--- app/src/main/java/jt/projects/perfectday/presentation/today/adapter/main/FactOfDayViewHolder.kt
+++ app/src/main/java/jt/projects/perfectday/presentation/today/adapter/main/FactOfDayViewHolder.kt
@@ -15,7 +15,6 @@ class FactOfDayViewHolder private constructor(
     )
 
     fun bind(item: TodayItem.FactOfDay) {
-        binding.root.isVisible = item.list.isNotEmpty()
-        binding.tvFactDescription.text = item.list.first().name + item.list.first().description
+
     }
 }
--- utils/src/main/java/jt/projects/utils/extensions/CoreExtensions.kt
+++ utils/src/main/java/jt/projects/utils/extensions/CoreExtensions.kt
@@ -2,13 +2,11 @@ package jt.projects.utils.extensions
 
 import android.view.View
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
 
 fun emptyString(): String = ""
 
 fun View.hideViewInRecycler() {
     this.isVisible = false
-    val params = this.layoutParams
-    params.width = 0
-    params.height = 0
-    this.layoutParams = params
+    this.layoutParams = RecyclerView.LayoutParams(0, 0)
 }
