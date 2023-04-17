2a2ad8a41f933e3ff68b3b680ec5f73e261b0fc9
EugeneZaukin <ezapach@yandex.ru>
2023-04-17 21:47:23 +0300

    Add note new item

--- app/src/main/java/jt/projects/perfectday/presentation/today/adapter/main/NoteViewHolder.kt
+++ app/src/main/java/jt/projects/perfectday/presentation/today/adapter/main/NoteViewHolder.kt
@@ -4,16 +4,18 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import jt.projects.perfectday.R
+import jt.projects.perfectday.databinding.ItemNoteBinding
 import jt.projects.perfectday.databinding.ItemScheduledEventBinding
 import jt.projects.utils.getAlertStringHowManyDaysBefore
 import jt.projects.utils.toStdFormatString
 
 class NoteViewHolder private constructor(
-    private val binding: ItemScheduledEventBinding
+    private val binding: ItemNoteBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
     constructor(parent: ViewGroup) : this(
-        ItemScheduledEventBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        ItemNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
     )
 
     fun bind(item: TodayItem.Notes, onClickDeleteNote: (Int) -> Unit) {
@@ -21,12 +23,9 @@ class NoteViewHolder private constructor(
 
 
         with(binding) {
-            tvHeader.text = "${data.name}"
-            tvDate.text = "${data.date.toStdFormatString()}"
+            tvHeader.text = data.name
+            tvDate.text = data.date.toStdFormatString()
             tvDescription.text = data.description
-
-            tvDaysToEvent.text = getAlertStringHowManyDaysBefore(data.date)
-            tvDaysToEvent.isVisible = false
 
             btnDelete.setOnClickListener {
                 onClickDeleteNote.invoke(data.id)
