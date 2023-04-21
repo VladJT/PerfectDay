ef052113a84e7c8a55eac998023149a36fb0367e
Rodion Usmanov <RQDerz@yandex.ru>
2023-04-21 22:56:15 +0300

    Исправлены баги: некорректное отображение меток заметок на календаре, вывод некорректного диалогового фрагмента при клике по дате без меток (теперь вызов диалогового фрагмента не происходит при отсутствии меток), клик по кнопкам изменения или удаления заметки не приводил к закрытию диалогового фрагмента.

--- app/src/main/java/jt/projects/perfectday/presentation/calendar/dateFragment/ChosenDateDialogFragment.kt
+++ app/src/main/java/jt/projects/perfectday/presentation/calendar/dateFragment/ChosenDateDialogFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -61,6 +63,12 @@ class ChosenDateDialogFragment(date: CalendarDate) : DialogFragment() {
         initRecyclerView()
         observeLoadingVisible()
         observeEditNote()
+
+        viewModel.message.observe(this, Observer {
+            it.getContentIfNotHandled()?.let {
+                dismiss()
+            }
+        })
     }
 
     private fun initViewModel() {
