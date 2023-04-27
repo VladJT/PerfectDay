76ce315217612a46dce7f20e0c88473808e618a6
VladJT <onlyrus@hotmail.com>
2023-04-27 15:28:30 +0300

    add translation

--- app/src/main/java/jt/projects/perfectday/presentation/today/adapter/main/FactOfDayViewHolder.kt
+++ app/src/main/java/jt/projects/perfectday/presentation/today/adapter/main/FactOfDayViewHolder.kt
@@ -4,13 +4,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import jt.projects.perfectday.core.translator.GoogleTranslator
-import jt.projects.perfectday.core.translator.TranslatorCallback
 import jt.projects.perfectday.databinding.ItemFactOfDayBinding
 import jt.projects.utils.extensions.hideViewInRecycler
 import jt.projects.utils.extensions.showViewInRecycler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import org.koin.java.KoinJavaComponent.getKoin
@@ -30,23 +30,18 @@ class FactOfDayViewHolder private constructor(
         }
         itemView.showViewInRecycler()
 
+        binding.tvFactDescription.text = item.list.first().description
+
+        val translator = getKoin().get<GoogleTranslator>()
+
+
         CoroutineScope(Dispatchers.Default).launch {
-            binding.tvFactDescription.text = item.list.first().description
-            val translator = getKoin().get<GoogleTranslator>()
             translator.isTranslationModelOk.onEach { modelOk ->
                 if (modelOk) {
-                    translator.translate(
-                        item.list.first().description,
-                        object : TranslatorCallback {
-                            override fun onSuccess(result: String?) {
-                                binding.tvFactDescription.text = result
-                            }
-
-                            override fun onFailure(result: String?) {
-                                binding.tvFactDescription.text = result
-                            }
-
-                        })
+                    translator.translate(item.list.first().description)
+                        .onEach {
+                            binding.tvFactDescription.text = it
+                        }.collect()
                 }
             }.collect()
         }
