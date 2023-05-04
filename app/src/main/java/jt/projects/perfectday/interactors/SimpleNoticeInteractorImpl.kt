5875aed9befc4e1f8c23a497b4f014cfcdd7ce6d
VladJT <onlyrus@hotmail.com>
2023-05-04 11:06:33 +0300

    добавил перевод интересного факта в интерактор

--- app/src/main/java/jt/projects/perfectday/interactors/SimpleNoticeInteractorImpl.kt
+++ app/src/main/java/jt/projects/perfectday/interactors/SimpleNoticeInteractorImpl.kt
@@ -2,17 +2,25 @@ package jt.projects.perfectday.interactors
 
 
 import jt.projects.model.DataModel
+import jt.projects.perfectday.core.translator.translateText
 import jt.projects.repository.network.facts.FactsRepository
 import java.time.LocalDate
 
 class SimpleNoticeInteractorImpl(private val repository: FactsRepository) {
+
     suspend fun getFactsByDate(date: LocalDate, factsCount: Int): List<DataModel.SimpleNotice> {
         val result = mutableListOf<DataModel.SimpleNotice>()
         for (i in 1..factsCount) {
             result.add(repository.getFactByDate(date))
         }
-        return result.distinctBy {
+        val distinctResult = result.distinctBy {
             it.description
         }
+
+        distinctResult.forEach {
+            it.description = it.description.translateText()
+        }
+
+        return distinctResult
     }
 }
