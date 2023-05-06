d3b36b9a591871c498e56bb1514b651f9453afca
EvgenNaumov <naumovev@mail.ru>
2023-05-06 15:26:48 +0300

    Запрос праздников с переводом

--- app/src/main/java/jt/projects/perfectday/interactors/HolidayInteractorImpl.kt
+++ app/src/main/java/jt/projects/perfectday/interactors/HolidayInteractorImpl.kt
@@ -1,6 +1,7 @@
 package jt.projects.perfectday.interactors
 
 import jt.projects.model.DataModel
+import jt.projects.perfectday.core.translator.translateText
 import jt.projects.repository.network.mapper.parseArrayDTOtoDataModel
 
 import jt.projects.repository.network.holiday.HolidayRepository
@@ -22,7 +23,19 @@ class HolidayInteractorImpl(
     suspend fun getCalendarificHolidayByDate(
         date: LocalDate,
         country: String = HOLIDAY_COUNTRY
-    ): List<DataModel.Holiday> =
-        parseCalendarificDTOtoDataModel(repository.getHolidayFromCalendarific(country, date))
+    ): List<DataModel.Holiday> {
+
+        val result = mutableListOf<DataModel.Holiday>()
+        val source =
+            parseCalendarificDTOtoDataModel(repository.getHolidayFromCalendarific(country, date))
+        source.forEach { result.add(it) }
+        val finalResult = result.distinctBy { it.description }
+        finalResult.forEach {
+            it.description = it.description?.translateText()
+            it.name = it.name.translateText()
+        }
+        return finalResult
+    }
+
 }
 
