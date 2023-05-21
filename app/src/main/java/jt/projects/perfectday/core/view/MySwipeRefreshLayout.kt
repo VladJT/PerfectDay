801b84f910f1c8202f66c32abf32bb9f3929d084
EugeneZaukin <ezapach@yandex.ru>
2023-05-21 10:56:12 +0300

    Fix recycler view scrool because swipe to refresh intercepts touch event

--- app/src/main/java/jt/projects/perfectday/core/view/MySwipeRefreshLayout.kt
+++ app/src/main/java/jt/projects/perfectday/core/view/MySwipeRefreshLayout.kt
@@ -1,10 +1,7 @@
 package jt.projects.perfectday.core.view
 
 import android.content.Context
-import android.os.Parcel
-import android.os.Parcelable
 import android.util.AttributeSet
-import android.util.Log
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.view.allViews
 import androidx.recyclerview.widget.RecyclerView
@@ -17,7 +14,12 @@ class MySwipeRefreshLayout : SwipeRefreshLayout {
 
     init {
         setOnChildScrollUpCallback { _, child ->
-            child is MotionLayout && child.progress == 1f
+            if (child is MotionLayout) {
+                val recycler = child.allViews.find { it is RecyclerView } as? RecyclerView
+                    ?: return@setOnChildScrollUpCallback false
+                child.progress == 1f || recycler.scrollState == RecyclerView.SCROLL_STATE_DRAGGING
+            } else
+                false
         }
     }
 }
