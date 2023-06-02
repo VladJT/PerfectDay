5ef7e6f0407f7f315081d9dd4a3e6f9064ea4b7f
EugeneZaukin <ezapach@yandex.ru>
2023-06-02 21:20:49 +0300

    Add all network model serializeble annotation

--- repository/src/main/java/jt/projects/repository/network/holiday/dto/calendarific/HolidayCalendarificDto.kt
+++ repository/src/main/java/jt/projects/repository/network/holiday/dto/calendarific/HolidayCalendarificDto.kt
@@ -5,16 +5,22 @@ import com.google.gson.annotations.SerializedName
 data class HolidayCalendarificDto(
     @SerializedName("canonical_url")
     val canonicalUrl: String,
+
     @SerializedName("description")
     val description: String,
+
     @SerializedName("locations")
     val locations: String,
+
     @SerializedName("name")
     val name: String,
+
     @SerializedName("primary_type")
     val type: String,
+
     @SerializedName("states")
     val states: String,
+
     @SerializedName("urlid")
     val urlid: String,
 )
--- repository/src/main/java/jt/projects/repository/network/holiday/dto/calendarific/Response.kt
+++ repository/src/main/java/jt/projects/repository/network/holiday/dto/calendarific/Response.kt
@@ -3,5 +3,6 @@ package jt.projects.repository.network.holiday.dto.calendarific
 import com.google.gson.annotations.SerializedName
 
 data class Response(
+    @SerializedName("holidays")
     val holidays: List<HolidayCalendarificDto>
 )
